72584a441c620c356786051188b13e91d114a944
cerro88 <bcerro245@gmail.com>
2023-06-15 20:58:18 +0200

    explicación del código

--- CursScripstBash_star/install_ubuntu.sh
+++ CursScripstBash_star/install_ubuntu.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 # script que instalará los paquetes básicos en ubuntu
+#un navegador y un editor de código
 
+#Función para detectar errores
 function usage(){
     echo "Error de instalación"
     exit 1
 }
+
+#Función para actualizar el sistema
 function actualizar(){
     apt update && apt upgrade
 }
 
+#Se comprueva que estamos ejecutando el scrip como usario root
 if [[ ${UID} -ne 0 ]]
 then
     echo "No eres el administrador"
     exit 1
 fi
 
-#se actualiza el sistema
+#se actualiza el sistema con la función que hemos creado, antes de instalar algo es recomendable actualizar
+#como se va usar más de una vez se ha creado una función
 actualizar
 #se instalan una serie de paquetes básicos para el terminal Linux
-apt install net-tools tree wget curl snap ssh git gparted
+apt install net-tools tree wget curl snap ssh git 
 #comprueva si ha habido algún error en la intalación de los paquetes
 if [[ ${?} -ne 0 ]]
 then
@@ -31,16 +37,17 @@ actualizar
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #se instala chrome
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-#si hay algun problema con la instalación
+# Si hay algún problema con la instalación de Chrome, corregir dependencias
 if [[ ${?} -ne 0 ]]
 then
 sudo apt-get install -f
-    #se vuelve a comprobar los errores
+    #se vuelve a comprobar los errores que pueda haber en la correción de dependencias
     if [[ ${?} -ne 0 ]]
     then
         usage
     fi
 fi
+#Se vuelve a actualizar para hacer otra instalación
 actualizar
 #instalación de visual studio code
 # Importar la clave GPG de Microsoft es parte del proceso para agregar el repositorio oficial de Visual Studio Code
@@ -49,17 +56,20 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 mv packages.microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 # se agrega el repositorio de Visual Studio Code al sistema
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" |  tee /etc/apt/sources.list.d/vscode.list
-
 # Actualiza la lista de paquetes e instala Visual Studio Code
 actualizar
+#Se instala visual Studio Code
 apt install code
-actualizar
+#Se vuelve a comprobar  que la instalación se ha hecho bien,y si hemos llegado al final del código, se emitirá
+#un mensaje de que todas las actualizaciones se han hecho
 if [[ ${?} == 0 ]]
 then
     echo "Las instalaciones se han hecho correctamente"
 else
     usage
 fi
+#Se vuelve a actualizar el sistema
+actualizar
 
 
 
